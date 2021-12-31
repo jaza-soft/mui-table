3255d3d2223a3f8199e1f1d06ef807e4c296f2f6
zahidraza <md-zahid.raza@outlook.com>
2021-12-31 16:24:30 +0530

    v0.2.28 - Minor NPE issue in i18n method

--- src/utils/translate.js
+++ src/utils/translate.js
@@ -1,7 +1,7 @@
 import lodashGet from 'lodash.get'
 
 const translate = (message, i18nMap = {}, args = {}) => {
-  let result = lodashGet(i18nMap, message, args?._)
+  let result = lodashGet(i18nMap, message, args?._) || ''
   const keys = Object.keys(args)
   keys.forEach((key) => {
     result = result.replaceAll(`%{${key}}`, args[key])
